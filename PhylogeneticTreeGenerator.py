7a2c1eaf132d8bc59c34787fe7da8be4a34bc711
ziele3920 <a.ziolkowski3920@wp.pl>
2017-12-08 12:18:51 +0100

    gitignore append

--- PhylogeneticTreeGenerator.py
+++ PhylogeneticTreeGenerator.py
@@ -1,7 +1,4 @@
-from ete3 import Tree, TreeStyle
+from ete3 import Tree
 
-t = Tree( "((a,b),c);" )
-circular_style = TreeStyle()
-circular_style.mode = "c" # draw tree in circular mode
-circular_style.scale = 20
-t.render("mytree.png", w=183, units="mm", tree_style=circular_style)
+t = Tree("((a,b),(dupa, (kupa, kal)));")
+t.show()
